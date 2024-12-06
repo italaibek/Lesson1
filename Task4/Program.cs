7c94d9b66f49149c7d8dd1487a1fbec4b2d82980
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-06 22:16:18 +0600

    lesson1:task4

---
+++ Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 6;
+int b = 77;
+int c = 9;
+int d = 4;
+int e = 99;
+
+int max = a;
+
+if (b>max){
+    max = b;
+}
+if (c>max){
+    max = c;
+}
+if (d>max){
+    max = d;
+}
+if (e>max){
+    max = e;
+}
+
+Console.WriteLine(max);
