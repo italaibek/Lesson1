ed74b61671711d4ac93937b39c220d0663a86d1b
Talaibek Iskakov <talaibek.iskakov@gmail.com>
2024-12-06 22:36:02 +0600

    lesson1:seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Кввадрат числа = {sqr}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Кввадрат числа = {sqr1}");
